851544c6ac04abd939fc0888d7221efd8e180620
Lihi <lihise9@gmail.com>
2021-12-01 08:53:30 +0200

    .

--- frontend/src/services/board.service.js
+++ frontend/src/services/board.service.js
@@ -3,12 +3,35 @@ import { storageService } from './async-storage.service'
 // import { socketService, SOCKET_EVENT_USER_UPDATED } from './socket.service'
 
 export const boardService = {
-    query
+    query,
+    remove,
+    add,
+    getById,
+
 }
 
 // Debug technique
 window.boardService = boardService
 
 function query() {
+    // return httpService.get(`board${queryStr}`)
     return storageService.query('board')
+}
+
+async function getById(boardId) {
+    // const board = await httpService.get(`board/${boardId}`)
+    const board = await storageService.get('board', boardId)
+    gWatchedUser = board;
+    return board;
+}
+
+function remove(boardId) {
+    // return httpService.delete(`board/${boardId}`)
+    return storageService.delete('board', boardId)
+}
+
+async function add(board) {
+    // const addedBoard = await httpService.post(`board`, board)
+    const addedBoard = storageService.post('board', board)
+    return addedBoard
 }
