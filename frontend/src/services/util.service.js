25684c9cd12980bd75258861d399d708a7e87ff3
Ortal <ortalcn@mta.ac.il>
2021-12-03 15:54:19 +0200

    fixing dnd between cards in same group

--- frontend/src/services/util.service.js
+++ frontend/src/services/util.service.js
@@ -1,5 +1,6 @@
 export const utilService = {
-    makeId
+    makeId,
+    applyDrag
 }
 
 function makeId(length = 5) {
@@ -10,3 +11,17 @@ function makeId(length = 5) {
     }
     return text
 }
+
+export const applyDrag = (arr, dragResult) => {
+    const { removedIndex, addedIndex, payload } = dragResult;
+    if (removedIndex === null && addedIndex === null) return arr;
+    const result = [...arr];
+    let itemToAdd = payload;
+    if (removedIndex !== null) {
+        itemToAdd = result.splice(removedIndex, 1)[0];
+    }
+    if (addedIndex !== null) {
+        result.splice(addedIndex, 0, itemToAdd);
+    }
+    return result;
+};
