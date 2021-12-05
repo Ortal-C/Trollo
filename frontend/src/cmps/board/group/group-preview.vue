1fec1c3b9931e8bb2a9991657aeb7216e766ffc7
Lihi <lihise9@gmail.com>
2021-12-05 18:17:26 +0200

    .

--- frontend/src/cmps/board/group/group-preview.vue
+++ frontend/src/cmps/board/group/group-preview.vue
@@ -66,7 +66,7 @@
           </g>
         </svg>
         <div class="archive-group" @click="removeGroup(group.id)">
-          <svg
+          <!-- <svg
             stroke="currentColor"
             fill="currentColor"
             stroke-width="0"
@@ -79,8 +79,8 @@
               fill-rule="evenodd"
               d="M13 2H1v2h12V2zM0 4a1 1 0 0 0 1 1v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v2zm2 1h10v9H2V5zm2 3h6V7H4v1z"
             ></path>
-          </svg>
-          Archive
+          </svg> -->
+          Archive this list
         </div>
       </div>
     </div>
