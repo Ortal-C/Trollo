0513c90c2a3ab05fb033737e11330d9b3752290c
Lihi <lihise9@gmail.com>
2021-12-05 21:56:19 +0200

    added membres imgs

--- frontend/src/cmps/board/card/card-preview.vue
+++ frontend/src/cmps/board/card/card-preview.vue
@@ -5,6 +5,11 @@
     <div v-if="!isEdit" class="card-preview" @click="cardDetails">
         <p>{{ card.title }}</p>
         <button class="edit-card" @click.stop="openEditCard">🖊</button>
+        <div class="card-icons">
+          <div v-for="member in card.members" :key="member._id">
+              <span><el-avatar :size="30" :src="member.imgUrl"></el-avatar></span>
+            </div>
+        </div>
     </div>
     <div class="card-edit-container" v-else>
       <form @change="editCard(group.id)">
