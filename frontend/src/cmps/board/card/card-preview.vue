5f4e76b8fd35767f25d2538f44adff673e8014bb
Lihi <lihise9@gmail.com>
2021-12-06 19:58:49 +0200

    .

--- frontend/src/cmps/board/card/card-preview.vue
+++ frontend/src/cmps/board/card/card-preview.vue
@@ -23,7 +23,7 @@
         <p class="card-title">{{ card.title }}</p>
         <div class="card-icons" v-if="card.style.size !== 'full'">
           <div class="card-info">
-            <span v-html="desc">{{ desc }}</span>
+            <span v-html="desc" v-if="card.description">{{ desc }}</span>
             <span v-if="card.dueDate" title="This card is due later">
               <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 24 24" style="fill: darkgray" >
                 <path d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z M 11 6 L 11 12.414062 L 15.292969 16.707031 L 16.707031 15.292969 L 13 11.585938 L 13 6 L 11 6 z"></path></svg>
