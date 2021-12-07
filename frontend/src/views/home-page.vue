485936bb69c3c103928c5c0d56361f26711c0933
Ortal <ortalcn@mta.ac.il>
2021-12-07 13:26:07 +0200

    delete backend from this repo

--- frontend/src/views/home-page.vue
+++ frontend/src/views/home-page.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home-page">
     <h1>Trollo</h1>
+    <!-- <p>
+      Trollo helps teams move work forward.
+      Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trollo.
+    </p>
+    <button>Try Demo version </button> -->
   </div>
 </template>
 
