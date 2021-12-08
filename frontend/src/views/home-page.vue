c1788845a9eada0478cc905f960134f4aa90b493
Michal <michalozeri88@gmail.com>
2021-12-08 13:31:21 +0200

    .

--- frontend/src/views/home-page.vue
+++ frontend/src/views/home-page.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home-page">
-    <h1>Trollo</h1>
-    <!-- <p>
-      Trollo helps teams move work forward.
-      Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trollo.
-    </p>
-    <button>Try Demo version </button> -->
+    <!-- <h1>Trollo</h1> -->
+    <div>
+      <div>
+        <h1>Trollo helps teams move work forward.</h1>
+        <p>
+          Collaborate, manage projects, and reach new productivity peaks. From
+          high rises to the home office, the way your team works is
+          unique—accomplish it all with Trollo.
+        </p>
+        <button>Try Demo version</button>
+      </div>
+      <img src="../src/assets/imgs/hero.png" alt="">
+    </div>
   </div>
 </template>
 
