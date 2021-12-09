acc2969eb82404d78e9d31a8423f94a797a77d2d
Michal <michalozeri88@gmail.com>
2021-12-09 10:11:09 +0200

    .

--- frontend/src/cmps/board/board-menu.vue
+++ frontend/src/cmps/board/board-menu.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="board-menu">
-    <!-- <header>
+    <header>
       <h3 class="title">Menu</h3>
       <a @click="closeMenu">
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24" style="fill: #2d2d2d" > <path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z" ></path> </svg>
@@ -42,13 +42,13 @@
     <hr />
     <main v-if="changeBg">
       <section  class="bg-img">
-        <!-- <input type="text" /> -->
+        <input type="text" />
         <div class="color" v-for="color in colors" :key="color" :value="color" :style="`background-color:${color}`" @click="updateStyle(color)"></div>
       </section>
       <section class="color-palette">
         <div class="color" v-for="color in colors" :key="color" :value="color" :style="`background-color:${color}`" @click="updateStyle(color)" ></div>
       </section>
-    </main> -->
+    </main>
   </div>
 </template>
 
