eaa8f7b5e7a727b6868b3c8df1d4f9ba0c3baac0
Ortal <ortalcn@mta.ac.il>
2021-12-10 13:42:12 +0200

    template update

--- frontend/src/cmps/board/board-menu.vue
+++ frontend/src/cmps/board/board-menu.vue
@@ -6,7 +6,7 @@
 		<header>
 			<h3 class="title">Menu</h3>
 			<a @click="closeMenu">
-				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 24 24" style="fill: #6b778c">
+				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="" viewBox="0 0 24 24" style="fill: #6b778c">
 					<path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path>
 				</svg>
 			</a>
@@ -86,8 +86,7 @@ import boardImgs from './board-imgs.vue'
 			return {
 				currAction: 'menu',
 				changeBg: false,
-				// colors: ['#cc0033', '#ff8000', '#ffbf00', '#00cc00', '#0099cc', '#66d9ff', '#bf80ff', '#ff66ff', '#ff4da6'],
-				colors: ['#eb3b5a', '#fa8231', '#f7b731', '#20bf6b', '#0fb9b1', '#45aaf2', '#4b7bec', '#a55eea', '#4b6584']
+				colors: ['#cc0033', '#ff8000', '#ffbf00', '#00cc00', '#0099cc', '#66d9ff', '#bf80ff', '#ff66ff', '#ff4da6'],
 			}
 		},
 		methods: {
