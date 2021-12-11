969cb5d96f53cda3b62650696e5ea45a6ba6b925
Ortal <ortalcn@mta.ac.il>
2021-12-11 22:10:04 +0200

    .

--- frontend/src/cmps/board/card/card-preview.vue
+++ frontend/src/cmps/board/card/card-preview.vue
@@ -66,11 +66,9 @@
                 <span class="popover-title"> {{ action.title }} </span>
                 <span v-html="getCloseSvg" @click="action.isOpen = false"></span>
               </div>
-              <component :card="card" :is="`card-${action.type}`"></component>
               <component
                 :card="card"
                 :is="`card-${action.type}`"
-                @addChecklist="closeActionModal"
                 @closeActionModal="closeActionModal"
                 @updateCard="updateCard"
               ></component>
@@ -99,14 +97,12 @@ export default {
           type: 'labels',
           isOpen: false,
           fa: `<i class="fas fa-tag"></i>`
-          // fa: ` <svg class="action-svg" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" > <path   d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16zM16 17H5V7h11l3.55 5L16 17z" ></path> </svg>`,
         },
         {
           title: 'Change members',
           type: 'members',
           isOpen: false,
           fa: `<i class="far fa-user"></i>`
-          // fa: `<svg class="action-svg" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" > <circle fill="none" cx="12" cy="7" r="3"></circle> <path   d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" ></path> </svg>`,
         },
         {
           title: 'Change cover',
@@ -119,29 +115,25 @@ export default {
           type: 'move',
           isOpen: false,
           fa: `<i class="fas fa-arrow-right"></i>`
-          // svg: `<svg class="action-svg" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"> <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"></path></svg>`,
         },
         {
           title: 'Copy',
           type: 'copy',
           isOpen: false,
           fa: `<i class="far fa-copy"></i>`
-          // svg: `<svg class="action-svg" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"> <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"></path></svg>`,
         },
         {
           title: 'Edit date',
           type: 'dates',
           isOpen: false,
           fa: `<i class="far fa-clock"></i>`
-          // fa: `<svg class="action-svg" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" > <path   fill-rule="evenodd"   d="M8 15A7 7 0 108 1a7 7 0 000 14zm8-7A8 8 0 110 8a8 8 0 0116 0z"   clip-rule="evenodd" ></path> <path   fill-rule="evenodd"   d="M7.5 3a.5.5 0 01.5.5v5.21l3.248 1.856a.5.5 0 01-.496.868l-3.5-2A.5.5 0 017 9V3.5a.5.5 0 01.5-.5z"   clip-rule="evenodd" ></path> </svg>`,
         },
         {
           title: 'Archive',
           type: 'archive',
           isOpen: false,
           fa: `<i class="fas fa-archive"></i>`
-          // fa: `<svg class="action-svg" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 14 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" > <path fill-rule="evenodd" d="M13 2H1v2h12V2zM0 4a1 1 0 0 0 1 1v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v2zm2 1h10v9H2V5zm2 3h6V7H4v1z" ></path> </svg>`,
-        },
+          },
       ],
     };
   },
