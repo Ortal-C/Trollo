ffca0dfde9ad5025a779eaf5bf0500952f8c870a
Michal <michalozeri88@gmail.com>
2021-12-11 16:47:31 +0200

    dashboard

--- frontend/src/cmps/board/charts/pie-chart.vue
+++ frontend/src/cmps/board/charts/pie-chart.vue
@@ -5,18 +5,27 @@ import { Pie } from 'vue-chartjs'
 export default {
   extends: Pie,
   props:['data'],
+   created() {
+    const members = Object.keys(this.data);
+    this.members = members;
+    const values = Object.values(this.data)
+    this.values = values
+  },
+  data() {
+    return {
+      members: null,
+      values: null,
+
+    }
+  },
   mounted() {
     // Overwriting base render method with actual data.
     this.renderChart({
-      labels: [
-        "Ortal Cohen",
-        "Lihi Sered",
-        "Michal Ozeri",
-      ],
+      labels: this.members,
       datasets: [
         {
           backgroundColor: ["#45aaf2","#26de81",'#fc5c65'],
-          data: this.data,
+          data: this.values,
         },
       ],
     });
---
+++ frontend/src/services/dashboard.service.js
@@ -0,0 +1,51 @@
+export const dashboardService = {
+    createLabelsMap,
+    setLabelsNames,
+    createMembersMap,
+}
+
+function createLabelsMap(board) {
+    var objMap = {}
+
+    board.groups.forEach(group => {
+        group.cards.forEach(card => {
+            card.labelsIds.forEach(label => {
+                objMap[label] ? objMap[label] += 1 : objMap[label] = 1
+            })
+        })
+    })
+    return objMap
+}
+
+function setLabelsNames(map, board) {
+    var res = {}
+    var keys = Object.keys(map)
+    var namedKeys = keys.map(key => {
+        var label = board.labels.find(currLabel => {
+            return currLabel.id === key
+        })
+        return label.title
+    })
+    var values = Object.values(map)
+        // namedKeys.forEach((key, idx) => {
+        //         res[key] = values[idx]
+        //     })
+    for (let i = 0; i < namedKeys.length; i++) {
+        res[namedKeys[i]] = values[i]
+    }
+    console.log('res', res);
+    return res
+}
+
+function createMembersMap(board) {
+    var objMap = {}
+
+    board.groups.forEach(group => {
+        group.cards.forEach(card => {
+            card.members.forEach(member => {
+                objMap[member.fullname] ? objMap[member.fullname] += 1 : objMap[member.fullname] = 1
+            })
+        })
+    })
+    return objMap
+}
