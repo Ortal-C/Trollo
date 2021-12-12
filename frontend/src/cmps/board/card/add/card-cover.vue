2487eefe66900284245bbfba9e203032f23e5e05
Lihi <lihise9@gmail.com>
2021-12-12 13:57:20 +0200

    .

--- frontend/src/cmps/board/card/add/card-cover.vue
+++ frontend/src/cmps/board/card/add/card-cover.vue
@@ -19,6 +19,7 @@
 <script>
 export default {
   name:'card-cover',
+  props: ['card'],
   data(){
     return{
       colors: ['#eb3b5a', '#fa8231', '#f7b731', '#20bf6b', '#0fb9b1', '#45aaf2', '#4b7bec', '#a55eea', '#4b6584', '#a5b1c2']
@@ -28,9 +29,9 @@ export default {
     board() {
       return this.$store.getters.board || null;
     },
-    card() {
-      return this.$store.getters.currCard
-    },
+    // card() {
+    //   return this.$store.getters.currCard
+    // },
     group() {
       return this.$store.getters.currGroup
     }
